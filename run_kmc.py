4778bc034f54f2a3c9e1ce53cfaf5231fb66e753
Chungsang Tom Lam <erinata@gmail.com>
2022-11-03 12:16:27 -0400

    add kmedoids

--- run_kmc.py
+++ run_kmc.py
@@ -2,6 +2,8 @@ import pandas
 import matplotlib.pyplot as pyplot
 
 from sklearn.cluster import KMeans
+from sklearn_extra.cluster import KMedoids
+
 
 from sklearn.metrics import silhouette_score
 
@@ -56,7 +58,39 @@ print(silhouette_result.index(max(silhouette_result))+2)
 
 
 
+def run_kmedoids(n, dataset):
+  machine = KMedoids(n_clusters=n)
+  machine.fit(dataset)
+  results = machine.predict(dataset)
+  centroids = machine.cluster_centers_
+  ssd = machine.inertia_
+  if n > 1:
+    silhouette = silhouette_score(dataset, machine.labels_, metric="euclidean")
+  else:
+    silhouette = 0
+  pyplot.scatter(dataset[:,0], dataset[:,1], c=results)
+  pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker="*", s = 200)
+  pyplot.savefig("scatterplot_kmedoids_" + str(n) + ".png")
+  pyplot.close()
+  return ssd, silhouette
 
+
+result = [run_kmedoids(i+1, dataset) for i in range(7)]
+# print(result)
+
+ssd_result = [ i[0] for i in result]
+pyplot.plot(range(1,8), ssd_result)
+pyplot.savefig("kmedoids_ssd.png")
+pyplot.close()
+
+
+silhouette_result = [ i[1] for i in result][1:]
+pyplot.plot(range(2,8), silhouette_result)
+pyplot.savefig("kmedoids_silhouette.png")
+pyplot.close()
+
+print(silhouette_result)
+print(silhouette_result.index(max(silhouette_result))+2)
 
 
 
